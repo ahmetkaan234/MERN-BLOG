7784ecb1e1ef3cea3a88031d4f86c6b5385138cd
Ahmet Kaan <ahmetkaan234@gmail.com>
2022-09-24 21:33:12 +0300

    url-config

--- src/redux/articleSlice.js
+++ src/redux/articleSlice.js
@@ -1,30 +1,32 @@
 import {createSlice,createAsyncThunk} from '@reduxjs/toolkit'
 import axios from 'axios'
 
+const BASEURL = "https://blogahmet.herokuapp.com";
+
 export const getArticleAsync = createAsyncThunk("article/getArticleAsync",async()=>{
-    const res = await axios(`http://localhost:5000/get`);
+    const res = await axios(`${BASEURL}/get`);
     return res.data;
 })
 
 export const postArticleAsync = createAsyncThunk("article/postArticleAsync",async(data)=>{
-        const res= await axios.post("http://localhost:5000/posts",data);
+        const res= await axios.post(`${BASEURL}/posts`,data);
         return res.data;
 
 })
 
 export const getDetailArticeAsync = createAsyncThunk("article/getDetailArticeAsync",async(slug)=>{
-        const res =await axios.get(`http://localhost:5000/${slug}`)
+        const res =await axios.get(`${BASEURL}/${slug}`)
         return  res.data;
 })
 
 
 export const deleteArticleAsync = createAsyncThunk("article/deleteArticleAsync",async(id)=>{
-    const res = await axios.delete(`http://localhost:5000/delete/${id}`)
+    const res = await axios.delete(`${BASEURL}/delete/${id}`)
     return res.data;
 })
 
 export const uptadeArticleAsync = createAsyncThunk("article/uptadeArticleAsync", async({id,data})=>{
-    const res =await axios.patch(`http://localhost:5000/article/${id}`,data)
+    const res =await axios.patch(`${BASEURL}/article/${id}`,data)
     return res.data;
 })
 
